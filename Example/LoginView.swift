f2bd4621b21a82a9c074c7ab142b820195abc5cd
Mohammad Alatrash <mkalatrash@gmail.com>
2021-03-31 10:18:57 +0300

    update the example

--- Example/LoginView.swift
+++ Example/LoginView.swift
@@ -10,29 +10,34 @@ import SwiftUI
 import ValidatedPropertyKit
 
 struct LoginView: View {
-    
     @Validated(!.isEmpty && .isEmail)
     var mailAddress = String()
-    
+
     @Validated(.range(8...))
     var password = String()
-    
+
+    @Validated(!.isEmpty && .range(4...))
+    var name: String = ""
+
     var body: some View {
         List {
             TextField("E-Mail", text: self.$mailAddress)
+                .shakeEffect(shakes: _mailAddress.invalidAttempts)
             TextField("Password", text: self.$password)
+
+            TextField("Name", text: self.$name)
+                .validation(self._name)
+
             Button(
                 action: {
-                    print("Login", self.mailAddress, self.password)
+                    _mailAddress.highlightIfNeeded()
+                    _name.highlightIfNeeded()
                 },
                 label: {
                     Text("Submit")
                 }
             )
-            .validated(
-                self._mailAddress,
-                self._password
-            )
+            .validated(self._password)
         }
         .listStyle(InsetGroupedListStyle())
     }
